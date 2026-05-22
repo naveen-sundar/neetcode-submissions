2bbd19b2376753b30dc59a1e76c72f2b048d1e72
Naveen Sundar M <148445571+naveen-sundar@users.noreply.github.com>
2026-05-22 15:12:40 +0530

    Add: sqrtx - submission-7

---
+++ Data Structures & Algorithms/sqrtx/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 1, x
+
+        while low <= high:
+            mid = (low + high) // 2
+            
+
+            if mid * mid > x:
+                high = mid - 1
+            elif mid * mid < x:
+                low = mid + 1
+            else:
+                return mid
+        return low - 1
